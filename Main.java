4440037e30579d48823e5996724fb65c68349ee5
Robbie Bise <theil.bise@sonarsource.com>
2022-12-13 11:23:34 -0600

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;
+
+public class Main
+{
+   public static void main(String[] arguments)
+   {
+      out.println("HELLO");
+      String a = null;
+      a = "asdf";
+      if (a != null) {out.println(a);}
+   }
+}
