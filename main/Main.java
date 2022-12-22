4359e1e2d71c05bb7d8d0f5bdeb899f6cdf0c939
Robbie Bise <theil.bise@sonarsource.com>
2022-12-22 13:28:34 -0600

    fix comment

--- main/Main.java
+++ main/Main.java
@@ -8,7 +8,7 @@ public class Main {
       String a = null;
       a = "asdf";
       if (a != null) {out.println(a);}
-      //TODO: something
+      //TODO: something else
 
       OldStuff oldStuff = new OldStuff();
       oldStuff.oldMethod();
